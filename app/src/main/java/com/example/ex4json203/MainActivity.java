e0ddf3f292d98294f1e5b4ee048f405f2648cbe8
lamnaourm <mohammed.lamnaour@gmail.com>
2022-06-07 16:43:43 +0100

    Recuperer une entreprise

--- app/src/main/java/com/example/ex4json203/MainActivity.java
+++ app/src/main/java/com/example/ex4json203/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Entreprise e = getEntreprise();
+
+
     }
 
     public String loadJSonFromRaw(int resId){
@@ -30,7 +38,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public Entreprise getEntreprise(){
-        return null;
+        Entreprise e = new Entreprise();
+        try {
+            String json = loadJSonFromRaw(R.raw.entreprise);
+            JSONObject obj = new JSONObject(json);
+            e.setRs(obj.getString("raisonSociale"));
+            e.setAdresse(obj.getString("adresse"));
+            JSONArray array = obj.getJSONArray("departement");
+            for(int i=0;i<array.length();i++){
+                JSONObject o = array.getJSONObject(i);
+                Departement d = new Departement();
+                d.setNom(o.getString("nom"));
+                d.setNbEmp(o.getInt("nbEmploye"));
+                e.getDeps().add(d);
+            }
+
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+
+
+        return e;
     }
 
 }
